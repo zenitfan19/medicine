ac73efa157a0c0bb8e9a52c118ffec83f94c7b8e
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-07-10 14:49:50 +0300

    transparent field

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,80 +8,100 @@ $(document).ready(function () {
 	});
 });
 
-$(document).on('click', '.main-center-left', function () {
-	if ($(document).find('.main-right').hasClass('active')) {
-		$(document).find('.main-center__label').removeClass('right');
-		$(document).find('.main-center__box').removeClass('right');
-		$(document).find('.main-center__label').addClass('left');
-		$(document).find('.main-center__box').addClass('left');
-		$(document).find('.main-center-bot').addClass('left');
-		$(document).find('.main-center__box2').addClass('left');
-		$(document).find('.main-center-right-mob').addClass('left');
-		$(document).find('.main-center-right-mob').removeClass('right');
-		$(document).find('.main-center__box2').removeClass('right');
-		$(document).find('.main-left').addClass('active');
-		$(document).find('.main').addClass('left');
-		$(document).find('.main').removeClass('right');
-		$(document).find('.main-bot').removeClass('right');
-		$(document).find('.main-bot').addClass('left');
-		setTimeout(function () {
-			$(document).find('.main-right').removeClass('active');
-		}, 100);
-		$(this).fadeOut('fast');
-		$(document).find('.main-center-right').fadeIn('fast');
-		$(document).find('.main-center-right-wrap').hide();
-	} else {
-		$(this).fadeOut('fast');
-		$(document).find('.main-center-right-wrap').fadeOut('fast');
-		$(document).find('.main-center__label').addClass('left');
-		$(document).find('.main-center__box').addClass('left');
-		$(document).find('.main-center-bot').addClass('left');
-		$(document).find('.main-center__box2').addClass('left');
-		$(document).find('.main-center-right-mob').addClass('left');
-		$(document).find('.main-center-right-mob').removeClass('right');
-		$(document).find('.main-left').addClass('active');
-		$(document).find('.main').addClass('left');
-		$(document).find('.main').removeClass('right');
-		$(document).find('.main-bot').removeClass('right');
-		$(document).find('.main-bot').addClass('left');
+$(document).on('click', '.main-center-left, .main-center-left-btn', function (event) {	
+	if( (screen.width > 767 && !(event.target.classList.contains("main-center-left-btn"))) || (screen.width <= 767 && (event.target.classList.contains("main-center-left-btn"))) ) {
+		if ($(document).find('.main-right').hasClass('active')) {
+			$(document).find('.main-center__label').removeClass('right');
+			$(document).find('.main-center__box').removeClass('right');
+			$(document).find('.main-center__label').addClass('left');
+			$(document).find('.main-center__box').addClass('left');
+			$(document).find('.main-center-bot').addClass('left');
+			$(document).find('.main-center__box2').addClass('left');
+			$(document).find('.main-center-right-mob').addClass('left');
+			$(document).find('.main-center-right-mob').removeClass('right');
+			$(document).find('.main-center__box2').removeClass('right');
+			$(document).find('.main-left').addClass('active');
+			$(document).find('.main').addClass('left');
+			$(document).find('.main').removeClass('right');
+			$(document).find('.main-bot').removeClass('right');
+			$(document).find('.main-bot').addClass('left');
+			setTimeout(function () {
+				$(document).find('.main-right').removeClass('active');
+			}, 100);
+			if($(this).hasClass('main-center-left-btn')) {
+
+			} else {
+				$(this).fadeOut('fast');
+			}			
+			$(document).find('.main-center-right').fadeIn('fast');
+			$(document).find('.main-center-right-wrap').hide();
+		} else {
+			if($(this).hasClass('main-center-left-btn')) {
+
+			} else {
+				$(this).fadeOut('fast');
+			}	
+			$(document).find('.main-center-right-wrap').fadeOut('fast');
+			$(document).find('.main-center__label').addClass('left');
+			$(document).find('.main-center__box').addClass('left');
+			$(document).find('.main-center-bot').addClass('left');
+			$(document).find('.main-center__box2').addClass('left');
+			$(document).find('.main-center-right-mob').addClass('left');
+			$(document).find('.main-center-right-mob').removeClass('right');
+			$(document).find('.main-left').addClass('active');
+			$(document).find('.main').addClass('left');
+			$(document).find('.main').removeClass('right');
+			$(document).find('.main-bot').removeClass('right');
+			$(document).find('.main-bot').addClass('left');
+		}
 	}
 });
 
-$(document).on('click', '.main-center-right', function () {
-	if ($(document).find('.main-left').hasClass('active')) {
-		$(document).find('.main-center__label').removeClass('left');
-		$(document).find('.main-center__box').removeClass('left');
-		$(document).find('.main-center-right-mob').removeClass('left');
-		$(document).find('.main-center-right-mob').addClass('right');
-		$(document).find('.main-center__box2').removeClass('left');
-		$(document).find('.main-center__label').addClass('right');
-		$(document).find('.main-center__box').addClass('right');
-		$(document).find('.main-right').addClass('active');
-		$(document).find('.main-center-bot').removeClass('left');
-		$(document).find('.main').removeClass('left');
-		$(document).find('.main').addClass('right');
-		$(document).find('.main-bot').removeClass('left');
-		$(document).find('.main-bot').addClass('right');
-		setTimeout(function () {
-			$(document).find('.main-left').removeClass('active');
-		}, 100);
-		setTimeout(function () {
+$(document).on('click', '.main-center-right, .main-center-right-btn', function (event) {	
+	if( (screen.width > 767 && !(event.target.classList.contains("main-center-right-btn"))) || (screen.width <= 767 && (event.target.classList.contains("main-center-right-btn"))) ) {
+		if ($(document).find('.main-left').hasClass('active')) {
+			$(document).find('.main-center__label').removeClass('left');
+			$(document).find('.main-center__box').removeClass('left');
+			$(document).find('.main-center-right-mob').removeClass('left');
+			$(document).find('.main-center-right-mob').addClass('right');
+			$(document).find('.main-center__box2').removeClass('left');
+			$(document).find('.main-center__label').addClass('right');
+			$(document).find('.main-center__box').addClass('right');
+			$(document).find('.main-right').addClass('active');
+			$(document).find('.main-center-bot').removeClass('left');
+			$(document).find('.main').removeClass('left');
+			$(document).find('.main').addClass('right');
+			$(document).find('.main-bot').removeClass('left');
+			$(document).find('.main-bot').addClass('right');
+			setTimeout(function () {
+				$(document).find('.main-left').removeClass('active');
+			}, 100);
+			setTimeout(function () {
+				$(document).find('.main-center__box2').addClass('right');
+			}, 150);
+			if($(this).hasClass('main-center-right-btn')) {
+
+			} else {
+				$(this).fadeOut('fast');
+			}	
+			$(document).find('.main-center-left').fadeIn('fast');
+			$(document).find('.main-center-left-wrap').hide();
+		} else {
+			if($(this).hasClass('main-center-right-btn')) {
+
+			} else {
+				$(this).fadeOut('fast');
+			}	
+			$(document).find('.main-center-left-wrap').fadeOut('fast');
+			$(document).find('.main-center__label').addClass('right');
+			$(document).find('.main-center__box').addClass('right');
+			$(document).find('.main-center-right-mob').addClass('right');
 			$(document).find('.main-center__box2').addClass('right');
-		}, 150);
-		$(this).fadeOut('fast');
-		$(document).find('.main-center-left').fadeIn('fast');
-		$(document).find('.main-center-left-wrap').hide();
-	} else {
-		$(this).fadeOut('fast');
-		$(document).find('.main-center-left-wrap').fadeOut('fast');
-		$(document).find('.main-center__label').addClass('right');
-		$(document).find('.main-center__box').addClass('right');
-		$(document).find('.main-center-right-mob').addClass('right');
-		$(document).find('.main-center__box2').addClass('right');
-		$(document).find('.main-right').addClass('active');
-		$(document).find('.main').addClass('right');
-		$(document).find('.main-bot').addClass('right');
-	}
+			$(document).find('.main-right').addClass('active');
+			$(document).find('.main').addClass('right');
+			$(document).find('.main-bot').addClass('right');
+		}
+	} 
 });
 
 $(document).on('click', '.main-center-bot', function () {
